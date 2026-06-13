291772c552b174a557b4035ee468a6b729a4daa3
Roshan <roshanvadapalli@gmail.com>
2026-06-13 10:59:41 +0530

    Initial project setup

---
+++ src/main/java/com/abcfoodzone/abc_food_zone/AbcFoodZoneApplication.java
@@ -0,0 +1,13 @@
+package com.abcfoodzone.abc_food_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcFoodZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcFoodZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abcfoodzone/abc_food_zone/AbcFoodZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcfoodzone.abc_food_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcFoodZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
